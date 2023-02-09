6389573861c74631124129859707a8f890b0064e
Varuj2002 <varuzhan.hovsepyan.dwin@gmail.com>
2023-02-09 12:26:08 +0400

    first commit

--- gpt3/src/App.js
+++ gpt3/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { CTA, Brand, Navbar } from './components';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT,
+  Header,
+} from "./container";
+import { CTA, Brand, Navbar } from "./components";
 
-import './App.css';
+import "./App.css";
 
 const App = () => (
-	<div className="App">
-		<div className="gradient__bg">
-			<Navbar />
-			<Header />
-		</div>
-		<Brand />
-		<WhatGPT3 />
-		<Features />
-		<Possibility />
-		<CTA />
-		<Blog />
-		<Footer />
-	</div>
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
 );
 
 export default App;
