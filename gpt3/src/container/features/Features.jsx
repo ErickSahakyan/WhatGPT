364f47ba14941493c9aece6abd971126f8249f15
Varuj2002 <varuzhan.hovsepyan.dwin@gmail.com>
2023-02-10 12:24:04 +0400

    first commit

--- gpt3/src/container/features/Features.jsx
+++ gpt3/src/container/features/Features.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './features.css'
+
+const featuresData = [
+  {
+    title: '',
+    text: ''
+  },
+  {
+    title: '',
+    text: ''
+  },
+  {
+    title: '',
+    text: ''
+  },
+  {
+    title: '',
+    text: ''
+  },
+]
+
 
 const Features = () => {
   return (
-	<div>Features</div>
+	<div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'></h1>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item, index) => (
+          <Features/>
+        ))}
+      </div>
+  </div>
   )
 }
 
